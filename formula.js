c685f3a85a81c09dcd652124de3b6b6b6f83078b
Casey Williams <caseywilliams@users.noreply.github.com>
2016-06-17 11:15:47 -0700

    Implement formula negation

--- formula.js
+++ formula.js
@@ -12,6 +12,9 @@ const print = multimethod()
   .dispatch(function (t) {
     return t.type
   })
+  .when('VariableOrConstant', function (t) {
+    return t.name
+  })
   .when('BinaryExpression', function (t) {
     return [
       print(t.left),
@@ -23,8 +26,9 @@ const print = multimethod()
       return operatorStrings[t.operator] + print(t.argument)
     } else throw new Error(`Unknown operator: ${t.operator}`)
   })
-  .when('ExpressionStatement', function (t) {
-    return '(' + print(t.expression) + ')'
+  .when('FunctionExpression', function (t) {
+    const args = t.arguments.map(print)
+    return t.name + '(' + args.join(',') + ')'
   })
   .when('Predicate', function (t) {
     if (t.arguments.length) {
@@ -32,25 +36,104 @@ const print = multimethod()
       return t.name + '(' + args.join(',') + ')'
     } else return t.name
   })
-  .when('FunctionExpression', function (t) {
-    const args = t.arguments.map(print)
-    return t.name + '(' + args.join(',') + ')'
+  .when('ExpressionStatement', function (t) {
+    return '(' + print(t.expression) + ')'
   })
   .when('QuantifiedExpression', function (t) {
     let out = (t.quantifier === 'Universal' ? 'A.' : 'E.')
     return out + print(t.variable) + ' ' + print(t.expression)
   })
+  .default(function (t) {
+    throw new Error(`Unrecognized type: ${t.type}`)
+  })
+
+const negationWrap = (t) => ({
+  type: 'UnaryExpression',
+  operator: 'Negation',
+  argument: t
+})
+
+const negate = multimethod()
+  .dispatch(function (t) {
+    if (!t || !t.type) throw new Error(`Invalid expression: ${JSON.stringify(t)}`)
+    return t.type
+  })
   .when('VariableOrConstant', function (t) {
-    return t.name
+    return negationWrap(t)
+  })
+  .when('UnaryExpression', function (t) {
+    if (t.operator === 'Negation') {
+      return t.argument
+    } else throw new Error(`Unknown operator: ${t.operator}`)
+  })
+  .when('FunctionExpression', function (t) {
+    return negationWrap(t)
+  })
+  .when('Predicate', function (t) {
+    return negationWrap(t)
+  })
+  .when('ExpressionStatement', function (t) {
+    const out = JSON.parse(JSON.stringify(t))
+    if (t.expression.type === 'BinaryExpression') {
+      if (t.expression.operator === 'Conjunction') {
+        out.expression = {
+          type: 'BinaryExpression',
+          operator: 'Disjunction',
+          left: negate(t.expression.left),
+          right: negate(t.expression.right)
+        }
+      } else if (t.expression.operator === 'Disjunction') {
+        out.expression = {
+          type: 'BinaryExpression',
+          operator: 'Conjunction',
+          left: negate(t.expression.left),
+          right: negate(t.expression.right)
+        }
+      } else if (t.expression.operator === 'Implication') {
+        out.expression = {
+          type: 'BinaryExpression',
+          operator: 'Conjunction',
+          right: negate(t.expression.right),
+          left: t.expression.left
+        }
+      }
+    }
+    return out
+  })
+  .when('BinaryExpression', function (t) {
+    return {
+      type: 'BinaryExpression',
+      operator: t.operator,
+      left: negate(t.left),
+      right: negate(t.right)
+    }
+  })
+  .when('QuantifiedExpression', function (t) {
+    const out = JSON.parse(JSON.stringify(t))
+    if (t.quantifier === 'Universal') {
+      out.quantifier = 'Existential'
+    } else out.quantifier = 'Universal'
+    out.expression = negate(t.expression)
+    return out
+  })
+  .default(function (t) {
+    throw new Error(`Unrecognized type: ${t.type}`)
   })
 
 export default class Formula {
   constructor (input) {
-    this.source = parse(input)
+    if (typeof input === 'string') {
+      this.source = parse(input)
+    } else if (typeof input === 'object') {
+      this.source = JSON.parse(JSON.stringify(input))
+    } else throw new Error(`Invalid formula input format: ${JSON.stringify(input)}`)
   }
 
   stringify () {
     return print(this.source)
   }
 
+  negate () {
+    return new Formula(negate(this.source))
+  }
 }
--- test/formula_test.js
+++ test/formula_test.js
@@ -1,7 +1,37 @@
 import test from 'ava'
 import Formula from '../formula'
+import parse from '../parse'
+
+test('Creation from an object', t => {
+  const f = new Formula(parse('E.x f(x) | A.y (!Q -> P(y)) & R'))
+  t.is(f.stringify(), 'E.x f(x) | A.y (!Q -> P(y)) & R')
+})
 
 test('Basic string output', t => {
   const f = new Formula('E.x f(x) | A.y (!Q -> P(y)) & R')
   t.is(f.stringify(), 'E.x f(x) | A.y (!Q -> P(y)) & R')
+})
+
+test('Negation of terms', t => {
+  t.is(new Formula('x').negate().stringify(), '!x')
+  t.is(new Formula('!x').negate().stringify(), 'x')
+  t.is(new Formula('f(x)').negate().stringify(), '!f(x)')
+  t.is(new Formula('!f(x)').negate().stringify(), 'f(x)')
+})
+
+test('Negation of predicates', t => {
+  t.is(new Formula('P(x)').negate().stringify(), '!P(x)')
+  t.is(new Formula('!P(x)').negate().stringify(), 'P(x)')
+})
+
+test('Negation of binary expressions', t => {
+  t.is(new Formula('P | Q').negate().stringify(), '!P | !Q')
+  t.is(new Formula('(P | Q)').negate().stringify(), '(!P & !Q)')
+  t.is(new Formula('(P & Q)').negate().stringify(), '(!P | !Q)')
+  t.is(new Formula('(P -> Q)').negate().stringify(), '(P & !Q)')
+})
+
+test('Negation of quantified expressions', t => {
+  t.is(new Formula('A.x f(x)').negate().stringify(), 'E.x !f(x)')
+  t.is(new Formula('E.x f(x)').negate().stringify(), 'A.x !f(x)')
 })
